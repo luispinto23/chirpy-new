709ae36927e48a09fc2d0b857c1d81e62d51a360
Luís Pinto <luispinto23@gmail.com>
2024-09-07 00:49:08 +0100

    add .env

--- users_handlers.go
+++ users_handlers.go
@@ -8,10 +8,16 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+type loginReq struct {
+	Email            *string `json:"email,omitempty"`
+	Password         *string `json:"password,omitempty"`
+	ExpiresInSeconds int     `json:"expires_in_seconds,omitempty"`
+}
+
 type userDto struct {
-	ID       int     `json:"id,omitempty"`
 	Email    *string `json:"email,omitempty"`
 	Password *string `json:"password,omitempty"`
+	ID       int     `json:"id,omitempty"`
 }
 
 func (cfg *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
@@ -44,18 +50,18 @@ func (cfg *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	response := userDto{
-		dbUser.ID,
-		&dbUser.Email,
-		nil,
+		ID:       dbUser.ID,
+		Email:    &dbUser.Email,
+		Password: nil,
 	}
 	respondWithJSON(w, http.StatusCreated, response)
 }
 
 func (cfg *apiConfig) login(w http.ResponseWriter, r *http.Request) {
-	var user userDto
+	var req loginReq
 
 	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&user)
+	err := decoder.Decode(&req)
 	if err != nil {
 		log.Printf("Error decoding body: %s", err)
 
@@ -63,27 +69,27 @@ func (cfg *apiConfig) login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if user.Email == nil || user.Password == nil {
+	if req.Email == nil || req.Password == nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	dbUser, err := cfg.db.GetUser(*user.Email)
+	dbUser, err := cfg.db.GetUser(*req.Email)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(dbUser.Password), []byte(*user.Password))
+	err = bcrypt.CompareHashAndPassword([]byte(dbUser.Password), []byte(*req.Password))
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, "")
 		return
 	}
 
 	response := userDto{
-		dbUser.ID,
-		&dbUser.Email,
-		nil,
+		ID:       dbUser.ID,
+		Email:    &dbUser.Email,
+		Password: nil,
 	}
 
 	respondWithJSON(w, http.StatusOK, response)
